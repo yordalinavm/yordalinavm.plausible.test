b673b4844b927f49c155e08b1e5e4927ec534c14
yordalinavm <yordalina.metodieva@insitex.com>
2024-03-20 14:39:13 +0200

    Initial commit

---
+++ src/pages/CatsPage.js
@@ -0,0 +1,12 @@
+// CatsPage.js
+import React from "react"
+const CatsPage = ({ cats }) => {
+    return (
+        <>
+            <h1>Cats Page</h1>
+            <p>{cats}</p>
+        </>
+    )
+}
+
+export default CatsPage
---
+++ src/pages/DogsPage.js
@@ -0,0 +1,12 @@
+
+import React from "react"
+const DogsPage = ({ dogs }) => {
+    return (
+        <>
+            <h1>Dogs Page</h1>
+            <p>{dogs}</p>
+        </>
+    )
+}
+
+export default DogsPage
