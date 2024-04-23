62939bc608d2471db4cb6433e9ee83d754d8ef10
yordalinavm <yordalina.metodieva@insitex.com>
2024-04-23 12:25:42 +0300

    prefix added

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/yordalinavm.plausible.test",
   siteMetadata: {
     title: `CatsAndDogs`,
     siteUrl: `https://www.yourdomain.tld`,
